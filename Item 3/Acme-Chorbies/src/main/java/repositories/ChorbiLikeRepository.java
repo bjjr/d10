a97b4783ce1784a21d9515209d7f18daef236f8b
bermundom <berny.sevillista@gmail.com>
2017-04-02 17:48:30 +0200

    Added some queries to ChorbiLike an Administrator repositories

--- Item 3/Acme-Chorbies/src/main/java/repositories/ChorbiLikeRepository.java
+++ Item 3/Acme-Chorbies/src/main/java/repositories/ChorbiLikeRepository.java
@@ -1,12 +1,21 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.ChorbiLike;
 
 @Repository
 public interface ChorbiLikeRepository extends JpaRepository<ChorbiLike, Integer> {
+
+	@Query("select c from ChorbiLike c where c.liker.id = ?1")
+	Collection<ChorbiLike> findChorbiLikesByLiker(int chorbiId);
+
+	@Query("select c from ChorbiLike c where c.liked.id = ?1")
+	Collection<ChorbiLike> findChorbiLikesByLiked(int chorbiId);
 
 }
