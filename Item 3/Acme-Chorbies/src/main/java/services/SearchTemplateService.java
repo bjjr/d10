f8c8cec674f45806e7cf843c613fae754dc3a99d
Alejandro Hall <alejandropolvillo@gmail.com>
2017-04-13 12:32:19 +0200

    Add the repository and service for SearchTemplate

--- Item 3/Acme-Chorbies/src/main/java/services/SearchTemplateService.java
+++ Item 3/Acme-Chorbies/src/main/java/services/SearchTemplateService.java
@@ -1,15 +1,19 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.SearchTemplateRepository;
-import domain.Coordinates;
+import domain.Chorbi;
+import domain.CoordinatesTemplate;
 import domain.SearchTemplate;
 
 @Service
@@ -26,14 +30,14 @@ public class SearchTemplateService {
 	public SearchTemplate create() {
 		final SearchTemplate searchTemplateResult = new SearchTemplate();
 
-		final Coordinates coordinates = new Coordinates();
-		coordinates.setCity(null);
-		coordinates.setCountry(null);
-		coordinates.setProvince(null);
-		coordinates.setState(null);
+		final CoordinatesTemplate coordinatesTemplate = new CoordinatesTemplate();
+		coordinatesTemplate.setCity(null);
+		coordinatesTemplate.setCountry(null);
+		coordinatesTemplate.setProvince(null);
+		coordinatesTemplate.setState(null);
 
 		searchTemplateResult.setAge(null);
-		searchTemplateResult.setCoordinates(coordinates);
+		searchTemplateResult.setCoordinatesTemplate(coordinatesTemplate);
 		searchTemplateResult.setGender(null);
 		searchTemplateResult.setKeyword(null);
 		searchTemplateResult.setRelationship(null);
@@ -42,7 +46,9 @@ public class SearchTemplateService {
 	}
 
 	public SearchTemplate save(final SearchTemplate searchTemplate) {
+
 		Assert.notNull(searchTemplate, "SearchTemplateService.save: The search template cannot be null");
+
 		Assert.isTrue(this.isAValidSearchTemplate(searchTemplate), "SearchTemplateService.save: The search template needs to be valid");
 
 		final SearchTemplate searchTemplateSaved = this.searchTemplateRepository.save(searchTemplate);
@@ -61,32 +67,56 @@ public class SearchTemplateService {
 	private Boolean isAValidSearchTemplate(final SearchTemplate searchTemplate) {
 		Boolean res = true;
 
-		if (searchTemplate.getGender() != null && (!searchTemplate.getGender().toUpperCase().equals("MAN") || !searchTemplate.getGender().toUpperCase().equals("WOMAN")))
+		if (searchTemplate.getGender() != null && (!searchTemplate.getGender().toUpperCase().equals("MAN") && !searchTemplate.getGender().toUpperCase().equals("WOMAN")))
 			res = false;
 
 		if (searchTemplate.getRelationship() != null
-			&& (!searchTemplate.getRelationship().toUpperCase().equals("ACTIVITIES") || !searchTemplate.getRelationship().toUpperCase().equals("FRIENDSHIP") || !searchTemplate.getRelationship().toUpperCase().equals("LOVE")))
+			&& (!searchTemplate.getRelationship().toUpperCase().equals("ACTIVITIES") && !searchTemplate.getRelationship().toUpperCase().equals("FRIENDSHIP") && !searchTemplate.getRelationship().toUpperCase().equals("LOVE")))
 			res = false;
 
 		return res;
 	}
 
+	public SearchTemplate findSearchTemplateByPrincipal() {
+		final Chorbi chorbi = this.chorbiService.findByPrincipal();
+
+		return chorbi.getSearchTemplate();
+	}
+
 	// Other business methods
 
-	//	//TODO: hacer cacheable
-	//	public Collection<Chorbi> search(final SearchTemplate searchTemplate) {
-	//
-	//		final Chorbi loggedChorbi = this.chorbiService.findByPrincipal();
-	//		Assert.notNull(loggedChorbi, "SearchTemplateService.search: The logged user cannot be null");
-	//
-	//		Assert.notNull(loggedChorbi.getCreditCard(), "SearchTemplateService.search: You need a valid creditcard in order to perform a search");
-	//
-	//		return this.findChorbiesBySearchTemplate(searchTemplate);
-	//	}
-	//
-	//	private Collection<Chorbi> findChorbiesBySearchTemplate(final SearchTemplate searchTemplate) {
-	//		return this.searchTemplateRepository.findChorbiesBySearchTemplate(searchTemplate.getGender(), searchTemplate.getRelationship(), searchTemplate.getCoordinates().getCountry(), searchTemplate.getCoordinates().getState(), searchTemplate
-	//			.getCoordinates().getProvince(), searchTemplate.getCoordinates().getCity());
-	//	}
+	// //TODO: hacer cacheable
+	public Collection<Chorbi> search(final SearchTemplate searchTemplate) {
 
+		final Chorbi loggedChorbi = this.chorbiService.findByPrincipal();
+		Assert.notNull(loggedChorbi, "SearchTemplateService.search: The logged user cannot be null");
+
+		Assert.notNull(loggedChorbi.getCreditCard(), "SearchTemplateService.search: You need a valid creditcard in order to perform a search");
+
+		return this.findChorbiesBySearchTemplate(searchTemplate);
+	}
+
+	private Collection<Chorbi> findChorbiesBySearchTemplate(final SearchTemplate searchTemplate) {
+		Collection<Chorbi> res = new ArrayList<>();
+
+		if (searchTemplate.getCoordinatesTemplate() != null) {
+			res.addAll(this.searchTemplateRepository.findChorbiesBySearchTemplate(searchTemplate.getGender(), searchTemplate.getRelationship(), searchTemplate.getCoordinatesTemplate().getCountry(), searchTemplate.getCoordinatesTemplate().getState(),
+				searchTemplate.getCoordinatesTemplate().getProvince(), searchTemplate.getCoordinatesTemplate().getCity(), searchTemplate.getKeyword()));
+		} else {
+			res.addAll(this.searchTemplateRepository.findChorbiesBySearchTemplateWithoutCoordinates(searchTemplate.getGender(), searchTemplate.getRelationship(), searchTemplate.getKeyword()));
+		}
+
+		Collection<Chorbi> aux = new ArrayList<>();   //I need to do this regarding ConcurrentModificationException
+		if (searchTemplate.getAge() != null) {
+			for (Chorbi c : res) {
+				Integer age = Years.yearsBetween(LocalDate.fromDateFields(c.getBirthdate()), LocalDate.now()).getYears();
+				if (!(age > (searchTemplate.getAge() - 5) && age < (searchTemplate.getAge() + 5))) {
+					aux.add(c);
+				}
+			}
+		}
+		res.removeAll(aux);
+
+		return res;
+	}
 }
